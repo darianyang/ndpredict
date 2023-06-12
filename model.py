42453c58717a2c35bd7de13436e859f4f6fbaed6
Darian T. Yang <dty7@pitt.edu>
2023-06-12 17:15:07 -0400

    got some RF parm opt going, testing RF classifer vs regressor

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ TODO:
     * feature selection using RFE: recursive feature elimination.
     * LDA could be useful here? Can predict labels or label probabilities for ROCAUC.
     * hyperparameter opt of RF and/or LDA
+    * do 10 fold CV to account for bad train/test splitting
 
 RF ROCAUC from paper: 0.96
 Using the out-of-the-box sklearn RF regressor on same feature set: ROCAUC is 0.87-0.89
@@ -19,7 +20,7 @@ I made the following changes to feature space:
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import roc_auc_score
@@ -91,22 +92,70 @@ label_encoder = LabelEncoder()
 y_encoded = label_encoder.fit_transform(y)
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_encoded, test_size=0.2)
+X_train, X_test, y_train, y_test = \
+    train_test_split(X_scaled, y_encoded, test_size=0.2, random_state=1)
+def rocauc_score(model):
+    """
+    Input model (e.g. RF regressor) and output ROCAUC score.
+    """
+    # fit the model
+    model.fit(X_train, y_train)
 
-# Create and fit the random forest regression model
-model = RandomForestRegressor()
-model.fit(X_train, y_train)
+    # Predict the probabilities on the test set
+    y_pred = model.predict(X_test)
 
-# Predict the probabilities on the test set
-y_pred = model.predict(X_test)
+    # Compute ROC AUC score
+    roc_auc = roc_auc_score(y_test, y_pred)
 
-# Compute ROC AUC score
-roc_auc = roc_auc_score(y_test, y_pred)
+    print("ROC AUC score:", roc_auc)
+    #print(dict(zip(feat_names, model.feature_importances_)))
 
-print("ROC AUC score:", roc_auc)
-#print(dict(zip(feat_names, model.feature_importances_)))
+    plt.bar(feat_names, model.feature_importances_)
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.show()
 
-plt.bar(feat_names, model.feature_importances_)
-plt.xticks(rotation=45, ha="right")
-plt.tight_layout()
-plt.show()
+#rocauc_score(RandomForestRegressor(random_state=1))
+rocauc_score(RandomForestClassifier(random_state=1))
+
+# # RF parameter opt
+# from sklearn.model_selection import RandomizedSearchCV
+# # create random grid
+# # Number of trees in random forest
+# n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# # Number of features to consider at every split
+# max_features = ['auto', 'sqrt']
+# # Maximum number of levels in tree
+# max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+# max_depth.append(None)
+# # Minimum number of samples required to split a node
+# min_samples_split = [2, 5, 10]
+# # Minimum number of samples required at each leaf node
+# min_samples_leaf = [1, 2, 4]
+# # Method of selecting samples for training each tree
+# bootstrap = [True, False]
+# # Create the random grid
+# random_grid = {'n_estimators': n_estimators,
+#                'max_features': max_features,
+#                'max_depth': max_depth,
+#                'min_samples_split': min_samples_split,
+#                'min_samples_leaf': min_samples_leaf,
+#                'bootstrap': bootstrap}
+
+# # Use the random grid to search for best hyperparameters
+# # First create the base model to tune
+# rf = RandomForestRegressor()
+# # Random search of parameters, using 3 fold cross validation, 
+# # search across 100 different combinations, and use all available cores
+# rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid, n_iter=80, cv=3, verbose=1, n_jobs=8)
+
+# # Fit the random search model
+# rf_random.fit(X_train, y_train)
+
+# # save and test out best parameters
+# print(rf_random.best_params_)
+best = {'n_estimators': 1600, 'min_samples_split': 10, 'min_samples_leaf': 4, 'max_features': 'sqrt', 'max_depth': 30, 'bootstrap': False}
+#best = {'n_estimators': 1800, 'min_samples_split': 10, 'min_samples_leaf': 4, 'max_features': 'sqrt', 'max_depth': 80, 'bootstrap': False}
+#rocauc_score(RandomForestRegressor(**rf_random.best_params_))
+#rocauc_score(RandomForestRegressor(**best))
+rocauc_score(RandomForestClassifier(**best))
